4beeb2523e94402ac6f1e65a61c1c08a9c1b42fe
zdb <2271738697@qq.com>
2019-10-28 20:41:19 +0800

    Signed-off-by: zdb <2271738697@qq.com>

---
+++ src/main/java/com/ncvt/manmapper/ManmapperApplication.java
@@ -0,0 +1,13 @@
+package com.ncvt.manmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManmapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManmapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncvt/manmapper/ManmapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncvt.manmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManmapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
